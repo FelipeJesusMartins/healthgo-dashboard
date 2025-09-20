fd824386f5b0ffb5cb0b827f9aa48d6e045d5ee7
FelipeJesusMartins <felipemartins1359@gmail.com>
2025-09-19 22:20:22 -0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+// Angular Material
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './app/dashboard/dashboard';
+import { FormsModule } from '@angular/forms';
+
+bootstrapApplication(DashboardComponent, {
+  providers: [
+    importProvidersFrom(
+      CommonModule,
+      MatTableModule,
+      MatInputModule,
+      MatButtonModule,
+      FormsModule
+    )
+  ]
+});
